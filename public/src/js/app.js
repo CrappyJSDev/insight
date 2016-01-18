4a033eba849043ad4b1e9dd9ae5f68c45dd8c2ed
Esotericizm <Esotericizm88@gmail.com>
2016-01-18 18:13:09 +1000

    Florincoin

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
-var defaultCurrency = localStorage.getItem('insight-currency') || 'DGB';
+var defaultCurrency = localStorage.getItem('insight-currency') || 'FLO';
 
 angular.module('insight',[
   'ngAnimate',
--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: '/views/block.html',
-      title: 'Digibyte Block '
+      title: 'Florincoin Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -17,7 +17,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: '/views/transaction.html',
-      title: 'Digibyte Transaction '
+      title: 'Florincoin Transaction '
     }).
     when('/', {
       templateUrl: '/views/index.html',
@@ -25,15 +25,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: '/views/block_list.html',
-      title: 'Digibyte Blocks solved Today'
+      title: 'Florincoin Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: '/views/block_list.html',
-      title: 'Digibyte Blocks solved '
+      title: 'Florincoin Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: '/views/address.html',
-      title: 'Digibyte Address '
+      title: 'Florincoin Address '
     }).
     when('/status', {
       templateUrl: '/views/status.html',
